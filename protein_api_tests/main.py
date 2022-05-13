408db982636be5ddf024a77c8ba1a21e6a0a6bab
Clay Kosonocky <90992096+kosonocky@users.noreply.github.com>
2022-05-13 09:04:48 -0700

    updates

--- protein_api_tests/main.py
+++ protein_api_tests/main.py
@@ -1,8 +1,10 @@
 import argparse
 from cgi import test
+from curses.panel import top_panel
 from traceback import print_tb
 import requests
 import json
+import pandas as pd
 
 # [ ] Add shell arguments
 # [ ] Add parser to pull out pdbids used in training set
@@ -12,10 +14,10 @@ import json
 class ProteinStructures():
     def __init__(self, pdbid_list):
         self._pdbid_list = [pdbid for pdbid in pdbid_list]
-        self._class = {}
-        self._architecture = {}
-        self._topology = {}
-        self._homologous_superfamily = {}
+        self._class_dict = {}
+        self._architecture_dict = {}
+        self._topology_dict = {}
+        self._homologous_superfamily_dict = {}
 
     def get_cath_info(self):
         for pdbid in self._pdbid_list:
@@ -40,27 +42,44 @@ class ProteinStructures():
                 cath_found = False
                 for i in range(len(json_data['data']['polymer_entity_instances'][0]['rcsb_polymer_instance_annotation'])):
                     if json_data['data']['polymer_entity_instances'][0]['rcsb_polymer_instance_annotation'][i]['type'] == 'CATH':
-                        self._class.setdefault(
+                        self._class_dict.setdefault(
                             f"Class {cath_data[0]['id']} ({cath_data[0]['name']})", 0)
-                        self._class[
+                        self._class_dict[
                             f"Class {cath_data[0]['id']} ({cath_data[0]['name']})"] += 1
-                        self._architecture.setdefault(
-                            f"Arch {cath_data[1]['id']} ({cath_data[1]['name']})", 0)
-                        self._architecture[
-                            f"Arch {cath_data[1]['id']} ({cath_data[1]['name']})"] += 1
-                        self._topology.setdefault(
-                            f"Top {cath_data[2]['id']} ({cath_data[2]['name']})", 0)
-                        self._topology[
-                            f"Top {cath_data[2]['id']} ({cath_data[2]['name']})"] += 1
-                        self._homologous_superfamily.setdefault(
-                            f"Hom {cath_data[3]['id']} ({cath_data[3]['name']})", 0)
-                        self._homologous_superfamily[
-                            f"Hom {cath_data[3]['id']} ({cath_data[3]['name']})"] += 1
+                        self._architecture_dict.setdefault(
+                            f"Architecture {cath_data[1]['id']} ({cath_data[1]['name']})", 0)
+                        self._architecture_dict[
+                            f"Architecture {cath_data[1]['id']} ({cath_data[1]['name']})"] += 1
+                        self._topology_dict.setdefault(
+                            f"Topology {cath_data[2]['id']} ({cath_data[2]['name']})", 0)
+                        self._topology_dict[
+                            f"Topology {cath_data[2]['id']} ({cath_data[2]['name']})"] += 1
+                        self._homologous_superfamily_dict.setdefault(
+                            f"Homologous Superfamily {cath_data[3]['id']} ({cath_data[3]['name']})", 0)
+                        self._homologous_superfamily_dict[
+                            f"Homologous Superfamily {cath_data[3]['id']} ({cath_data[3]['name']})"] += 1
                         cath_found = True
                 if cath_found == False:
                     print(f"CATH Hierarchy Data not found for {pdbid}")
             except:
                 print(f"PDBID: {pdbid} does not exist")
+
+    def convert_dictionaries_to_dataframes(self):
+        self._class_df = pd.DataFrame.from_dict(
+            data=self._class_dict, orient='index', columns=['Count'])
+        self._architecture_dict = pd.DataFrame.from_dict(
+            data=self._architecture_dict, orient='index', columns=['Count'])
+        self._topology_dict = pd.DataFrame.from_dict(
+            data=self._topology_dict, orient='index', columns=['Count'])
+        self._homologous_superfamily_dict = pd.DataFrame.from_dict(
+            data=self._homologous_superfamily_dict, orient='index', columns=['Count'])
+
+    def convert_dfs_to_percentages(self):
+        grouped_dfs = (self._class_df, self._architecture_dict,
+                       self._topology_dict, self._homologous_superfamily_dict)
+        for df in grouped_dfs:
+            df['Percentage'] = df['Count'].divide(int(df.sum()))
+            print(df)
 
 
 def main():
@@ -68,11 +87,13 @@ def main():
                           '4BN2.A', '4B8Y.A', '5B8A.A', '7C6D.A', '7C2A.A', '2A1A.A', '5AB2.A', '6CE1.A']
     proteins = ProteinStructures(example_pdbid_list)
     proteins.get_cath_info()
-    print('Counts:', '\n')
-    print(proteins._class, '\n')
-    print(proteins._architecture, '\n')
-    print(proteins._topology, '\n')
-    print(proteins._homologous_superfamily, '\n')
+    proteins.convert_dictionaries_to_dataframes()
+    proteins.convert_dfs_to_percentages()
+    # print('Counts:', '\n')
+    # print(proteins._class_dict, '\n')
+    # print(proteins._architecture_dict, '\n')
+    # print(proteins._topology_dict, '\n')
+    # print(proteins._homologous_superfamily_dict, '\n')
     pass
 
 
